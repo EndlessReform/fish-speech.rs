da8ba051c21f42fd9a391ebc8511314b29fae94c
Jacob Keisling <contact@keisling.me>
2024-09-19 21:15:15 -0500

    WIP Add basic sampling

--- fish_speech_core/src/bin/llama_generate.rs
+++ fish_speech_core/src/bin/llama_generate.rs
@@ -1,43 +1,89 @@
-use candle_core::{DType, Device, IndexOp, NdArray, Result, Tensor, D};
-use candle_nn::{Module, VarBuilder};
+use candle_core::{DType, Device, IndexOp, Result, Tensor, D};
+use candle_nn::{ops::softmax_last_dim, Module, VarBuilder};
 use candle_transformers::generation::{LogitsProcessor, Sampling};
+use candle_transformers::utils::apply_repeat_penalty;
+use clap::Parser;
 use fish_speech_core::models::text2semantic::{BaseModelArgs, DualARTransformer};
 use std::time::Instant;
+
+/// For debugging purposes
+fn print_logprobs(logits: &Tensor) -> Result<()> {
+    let mut lp: Vec<(usize, f32)> = softmax_last_dim(&logits.flatten_all()?)?
+        .to_vec1::<f32>()?
+        .iter()
+        .enumerate()
+        .map(|(idx, p)| (idx, *p))
+        .collect();
+    lp.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap_or(std::cmp::Ordering::Equal));
+    lp.reverse();
+    // println!("Top logprobs: {:?}", lp.iter().take(10));
+    println!("Top logprobs: {:?}", &lp[..5]);
+    Ok(())
+}
+
+fn apply_rep_pen(
+    logits: &Tensor,
+    tokens: &[u32],
+    rep_pen: f32,
+    repeat_last_n: usize,
+) -> Result<Tensor> {
+    if rep_pen == 1. {
+        Ok(logits.clone())
+    } else {
+        let start_at = tokens.len().saturating_sub(repeat_last_n);
+        apply_repeat_penalty(&logits, rep_pen, &tokens[start_at..])
+    }
+}
 
 fn decode_one_token_ar(
     model: &mut DualARTransformer,
     logits_processor: &mut LogitsProcessor,
     x: &Tensor,
     input_pos: usize,
-    previous_tokens: &Tensor,
+    previous_tokens: Option<&Tensor>,
+    sampling_args: &SamplingArgs,
     write_debug_output: bool,
-) -> Result<Vec<u32>> {
+) -> Result<Tensor> {
     let (logits, hidden_states) = model.forward_generate(&x, input_pos)?;
-    if write_debug_output {
-        logits.write_npy("first_token_out_rs.npy").unwrap();
-        hidden_states.write_npy("first_hidden_rs.npy").unwrap();
-    }
+    let logits = logits.flatten_all()?;
 
-    let mut codebooks: Vec<u32> = vec![logits_processor.sample(&logits.flatten_all()?)?];
+    // print_logprobs(&logits)?;
+    let logits_adj = match previous_tokens {
+        Some(ctxt) => apply_rep_pen(
+            &logits,
+            &ctxt.i((0, ..))?.to_vec1()?,
+            sampling_args.repetition_penalty,
+            16,
+        )?,
+        None => logits,
+    };
+    let semantic_token = logits_processor.sample(&logits_adj)?;
+    let mut codebooks = vec![semantic_token];
     model.clear_fast_layer_caches();
 
     let mut x = hidden_states;
-    // println!("x shape: {:?}", x.shape());
     for codebook_idx in 0..model.cfg.num_codebooks {
         // TODO: Figure out what the heck input_pos is
-        let logits = model.forward_generate_fast(&x, codebook_idx)?;
-        if write_debug_output {
-            logits.write_npy(format!("fast_codebook_{}_logits_rust.npy", codebook_idx))?;
-        }
-        // TODO: Handle previous_tokens!
-        let a = logits_processor.sample(&logits.flatten_all()?)?;
+        let logits = model
+            .forward_generate_fast(&x, codebook_idx)?
+            .flatten_all()?;
+
+        let logits_adj = match previous_tokens {
+            Some(ctxt) => apply_rep_pen(
+                &logits,
+                &ctxt.i((codebook_idx + 1, ..))?.to_vec1()?,
+                sampling_args.repetition_penalty,
+                16,
+            )?,
+            None => logits,
+        };
+        let a = logits_processor.sample(&logits_adj.flatten_all()?)?;
         // println!("Codebook shape: {:?}", prev_codes[codebook_idx + 1].shape());
         let a_tensor = Tensor::from_slice(&[a], 1, x.device())?;
         x = model.fast_embeddings.forward(&a_tensor)?.unsqueeze(0)?;
         codebooks.push(a);
     }
-    // Tensor::from_vec(codebooks, model.cfg.num_codebooks + 1, x.device())?.unsqueeze(D::Minus1)
-    Ok(codebooks)
+    Tensor::from_vec(codebooks, model.cfg.num_codebooks + 1, x.device())?.unsqueeze(D::Minus1)
 }
 
 /// Takes a conditioning sequence as input and generates as many tokens as requested
@@ -46,13 +92,27 @@ fn generate(
     prompt: &Tensor,
     max_new_tokens: usize,
     im_end_id: Option<u32>,
+    sampling_args: &SamplingArgs,
 ) -> Result<Tensor> {
-    // Using deterministic sampling for debugging purposes.
-    // TODO: Make this configurable in API
-    let mut logits_processor = LogitsProcessor::from_sampling(0, Sampling::ArgMax);
+    let im_end_id = im_end_id.unwrap_or(4);
+
+    let mut logits_processor = LogitsProcessor::from_sampling(
+        0,
+        Sampling::TopP {
+            temperature: sampling_args.temp,
+            p: sampling_args.top_p,
+        },
+    );
     let start_pp = Instant::now();
-    let mut cur_token =
-        decode_one_token_ar(model, &mut logits_processor, prompt, 0, prompt, false)?;
+    let mut cur_token = decode_one_token_ar(
+        model,
+        &mut logits_processor,
+        prompt,
+        0,
+        None,
+        &sampling_args,
+        false,
+    )?;
     let dt = start_pp.elapsed();
     let mut input_pos = prompt.dim(D::Minus1)?;
     println!(
@@ -61,38 +121,87 @@ fn generate(
         input_pos as f64 / dt.as_secs_f64()
     );
 
-    let mut previous_tokens = vec![Tensor::from_vec(cur_token.clone(), 5, prompt.device())?];
-    println!("First token: {:?}", cur_token);
+    let mut previous_tokens = cur_token.clone();
+    // println!(
+    //     "First tokens: {:?}",
+    //     cur_token.flatten_all()?.to_vec1::<u32>()?
+    // );
 
     let start_decode = Instant::now();
     for i in 1..max_new_tokens {
         // TODO: handle window in inference code.
         // WTF does it do?!
         // Thank god for KV cache
-        let input_token = Tensor::from_vec(cur_token, (5, 1), prompt.device())?.contiguous()?;
         let next_token = decode_one_token_ar(
             model,
             &mut logits_processor,
-            &input_token,
+            &cur_token,
             input_pos,
-            &input_token,
+            Some(&previous_tokens),
+            sampling_args,
             false,
         )?;
-        println!("Token {}: {:?}", i + 1, next_token);
-        previous_tokens.push(Tensor::from_vec(next_token.clone(), 5, prompt.device())?);
+        // next_token.flatten_all()?.to_vec1::<u32>()
+        println!("Token {}", i + 1);
+        // previous_tokens.push(Tensor::from_slice(&next_token[1..], 4, prompt.device())?);
+        previous_tokens = Tensor::cat(&[previous_tokens, next_token.clone()], D::Minus1)?;
+        // println!("Items: {:?} ", next_token.i((0, 0))?.to_vec0::<u32>()?);
+        if let Some(semantic_token) = next_token.i((0, 0))?.to_vec0::<u32>().ok() {
+            if semantic_token == im_end_id {
+                break;
+            }
+        }
         input_pos += 1;
         cur_token = next_token;
     }
     let dt = start_decode.elapsed();
     println!(
         "{} tokens generated ({:.2} tokens/s)",
-        previous_tokens.len(),
-        previous_tokens.len() as f64 / dt.as_secs_f64()
+        previous_tokens.dim(1)?,
+        previous_tokens.dim(1)? as f64 / dt.as_secs_f64()
     );
-    Tensor::cat(&previous_tokens, D::Minus1)
+    // Tensor::cat(&previous_tokens, 0)?.reshape((4, ()))
+    previous_tokens.i((1.., ..))
+}
+
+struct SamplingArgs {
+    pub temp: f64,
+    pub top_p: f64,
+    pub repetition_penalty: f32,
+}
+
+/// A simple program to process some text with various settings
+#[derive(Parser, Debug)]
+#[command(
+    author = "Jacob Keisling <jacob@keisling.me>",
+    version = "0.1",
+    about = "Generates codebooks for Fish Speech"
+)]
+struct Args {
+    /// Temperature for sampling
+    #[arg(long, default_value_t = 0.7)]
+    temp: f64,
+
+    /// Top-p sampling parameter
+    #[arg(long, default_value_t = 0.7)]
+    top_p: f64,
+
+    /// Penalty for repetition
+    #[arg(long, default_value_t = 1.2)]
+    repetition_penalty: f32,
+
+    /// Text to process (required)
+    #[arg(long)]
+    text: String,
 }
 
 fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let sampling_args = SamplingArgs {
+        temp: args.temp,
+        top_p: args.top_p,
+        repetition_penalty: args.repetition_penalty,
+    };
     // TODO: Read config from checkpoint folder w/ serde; Fish Speech 1.4 support
     let config = BaseModelArgs::fish_speech_1_2();
     // TODO: Tokenization and preprocessing
@@ -111,7 +220,7 @@ fn main() -> anyhow::Result<()> {
     let mut model = DualARTransformer::load(&vb, &config, 5).unwrap();
     println!("Model loaded");
 
-    let res = generate(&mut model, &example_input, 100, Some(4))?;
+    let res = generate(&mut model, &example_input, 100, Some(4), &sampling_args)?;
     res.write_npy("out.npy")?;
     Ok(())
 }
